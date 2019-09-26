4d55fe39f4b820aae37d7cce33314a317209848c
Nikolas147 <mister.pim97@yandex.ru>
2019-09-26 21:29:40 +0300

    New Add Table View

---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,37 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Aleksey on 26/09/2019.
+//  Copyright © 2019 Aleksey. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+    static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: place))
+        }
+        
+        return places
+    }
+    
+    
+}
